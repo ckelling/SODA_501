e850b3341e4eb96dc608c05ed46e5ac7203566fb
So Young Park <sup97@psu.edu>
2018-04-12 11:49:15 -0400

    simple network characteristics

--- src/08_network_analysis.R
+++ src/08_network_analysis.R
@@ -42,15 +42,38 @@ edges <- select(edges, from, to, weight)
 head(edges)
 
 #build network
+
 #travel_network <- network(edges, vertex.attr = nodes, matrix.type = "edgelist", ignore.eval = FALSE)
 #plot(travel_network, vertex.cex = 1, mode = "circle")
 
 #detach(package:network)
 #rm(travel_network)
-#library(igraph)
-#travel_igraph <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
+library(igraph)
+travel_igraph <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
 #plot(travel_igraph, layout = layout_with_graphopt, edge.arrow.size = 0.2)
 
 library(visNetwork)
 library(networkD3)
 visNetwork(nodes, edges)
+
+# network analysis
+
+#average distance (how many edges to reach B from A on average)
+mean_distance(travel_igraph, directed=T)
+distance <- distances(travel_igraph)
+plot(distance)
+
+#degree
+degree <- degree(travel_igraph, mode="in")
+plot(degree)
+
+# Closeness (centrality based on distance to others in the graph)
+#Inverse of the node’s average geodesic distance to others in the network.
+
+closeness <- closeness(travel_igraph, mode="all", weights=NA) 
+plot(closeness)
+#Betweenness (centrality based on a broker position connecting others)
+#Number of geodesics that pass through the node or the edge.
+
+betweenness <- betweenness(travel_igraph, directed=T, weights=NA)
+plot(betweenness)
